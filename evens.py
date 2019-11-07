711005de1fb6c45ce5f919035b14db2a90af4f78
Rupert <rupertjhewson@gmail.com>
2019-11-07 17:23:25 +0000

    refactor

--- evens.py
+++ evens.py
@@ -1,12 +1,12 @@
 def is_even(number):
-    return number % 2 == 0 
+    return number % 2 == 0
 
 def even_number_of_evens(numbers):
-  
+
    evens = sum([1 for n in numbers if is_even(n)])
    return False if evens == 0 else is_even(evens)
-    
-assert even_number_of_evens([]) == False, "No numbers"  
+
+assert even_number_of_evens([]) == False, "No numbers"
 assert even_number_of_evens([2, 4]) == True, "Two even numbers"
 assert even_number_of_evens([2]) == False, "One even number"
 assert even_number_of_evens([1,3,9]) == False, "Three odd numbers"
@@ -14,4 +14,30 @@ assert even_number_of_evens([1,3,9]) == False, "Three odd numbers"
 
 print("All tests passed!")
 
+# def is_even(number):
+#    return number % 2 == 0
+
+# def even_number_of_evens(numbers):
+
+#     evens == 0
+#     for n in mumbers:
+#         if is_even(n):
+#             evens += 1
+#
+#    if evens == 0:
+#        return False
+#    else:
+
+#        return is_even(evens)
+
+# assert even_number_of_evens([]) == False, "No numbers"
+# assert even_number_of_evens([2, 4]) == True, "Two even numbers"
+# assert even_number_of_evens([2]) == False, "One even number"
+# assert even_number_of_evens([1,3,9]) == False, "Three odd numbers"
+
+
+
+# print("All tests passed!")
+
+# Example of refactoring, upper code is refactored from lower
 
